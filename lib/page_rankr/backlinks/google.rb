b72ef7335136c5f5d26316b4fa1842f5aaedfca2
Dru Ibarra <Druwerd@gmail.com>
2010-10-27 20:49:20 -0700

    use google's ajax search api to get backlinks

--- lib/page_rankr/backlinks/google.rb
+++ lib/page_rankr/backlinks/google.rb
@@ -1,15 +1,20 @@
 require 'cgi'
+require 'json'
 
 module PageRankr
   class Backlinks < Tracker
     class Google < Backlink
-      def url(site)
-        "http://www.google.com/search?q=link%3A%22#{CGI.escape(site)}%22"
+      
+      # overloaded to use Google's AJAX search API
+      # http://code.google.com/apis/ajaxsearch/documentation/
+      def initialize(site)
+        @backlinks = clean JSON.parse( open( url(site)).read )["responseData"]["cursor"]["estimatedResultCount"].to_s
       end
       
-      def xpath
-        "//div[@id='resultStats']/text()"
+      def url(site)
+        "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=link%3A#{CGI.escape(site)}"
       end
+
     end
   end
 end
