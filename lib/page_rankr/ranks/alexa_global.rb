2cde9d39c0bcd26cfaf568c489a5517907163383
Allen Madsen <blatyo@gmail.com>
2011-02-23 11:10:27 -0500

    Treat Alexa Ranks returned for incorrect site as if no rank were returned.

--- lib/page_rankr/ranks/alexa_global.rb
+++ lib/page_rankr/ranks/alexa_global.rb
@@ -6,6 +6,12 @@ module PageRankr
     class AlexaGlobal
       include Rank
       
+      # Alexa may sometimes return a result for the incorrect site and thus it is necessary to check if
+      # the results returned are for the site we want.
+      #
+      # For example, slocourts.net returns results for ca.gov, presumably because www.slocourts.ca.gov redirects
+      # to slocourts.net. Clearly something is wrong with how Alexa handles this case and so in the event this
+      # happens we treat the results as if there were no results.
       def xpath
          "//popularity[contains(@url, '#{@site.domain}')]/@text"
       end
---
+++ spec/edge_cases_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe PageRankr do
+  describe "#ranks" do
+    # Alexa may sometimes return a result for the incorrect site and thus it is necessary to check if
+    # the results returned are for the site we want.
+    #
+    # For example, slocourts.net returns results for ca.gov, presumably because www.slocourts.ca.gov redirects
+    # to slocourts.net. Clearly something is wrong with how Alexa handles this case and so in the event this
+    # happens we treat the results as if there were no results.
+    describe "when Alexa returns results for the incorrect site" do
+      subject{ PageRankr.ranks("slocourts.net", :alexa_us, :alexa_global) }
+      
+      it{ should have_key(:alexa_us) }
+      it{ should have_key(:alexa_global) }
+      
+      it{ subject[:alexa_us].should be_nil }
+      it{ subject[:alexa_global].should be_nil }
+    end
+  end
+end
