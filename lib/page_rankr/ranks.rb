28a5f36d7145afe70cb0766229f5d72c4637b00b
Ivan Danci <bah4uk@gmail.com>
2014-10-30 00:00:27 +0200

    Add domain authority rank

--- lib/page_rankr/ranks.rb
+++ lib/page_rankr/ranks.rb
@@ -4,12 +4,13 @@ require File.expand_path("../ranks/alexa_global", __FILE__)
 require File.expand_path("../ranks/alexa_country", __FILE__)
 require File.expand_path("../ranks/google", __FILE__)
 require File.expand_path('../ranks/moz_rank', __FILE__)
+require File.expand_path('../ranks/domain_authority', __FILE__)
 require File.expand_path('../ranks/page_authority', __FILE__)
 
 module PageRankr
   class Ranks
     include Trackers
-    
+
     alias_method :rank_trackers, :site_trackers
   end
 end
